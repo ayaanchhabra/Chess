883afea0881d55387453b8a57816bfe6b0d55c8b
mitansh-cmyk <mitanshsehgal01@gmial.com>
2026-07-21 19:50:22 +0530

    kuch changes

--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int power(int x,int n){
+    if(n==1){
+        return x;
+    }
+    return x * power(x,n-1);
+}
+
 int main(){
     int n;
     cin>>n;
