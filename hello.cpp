50c0a05b16c079e0247f01108d92ed95c20e94ea
ayaanchhabra <ayaanchhabraofficial@gmail.com>
2026-07-24 18:40:31 +0530

    yelo

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int x;
+    cin >> x;
+
+    cout << x * 2;
+
+    return 0;
+}
