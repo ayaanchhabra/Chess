af74d17260cad30027c69c1cbb76c454c6fea753
ayaanchhabra <ayaanchhabraofficial@gmail.com>
2026-07-24 18:55:07 +0530

    hello

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+class Node{
+    public:
+    int data;
+    Node *next;
 
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
 
-    int x;
-    cin >> x;
-
-    cout << x * 2;
-
-    return 0;
+    vector<int>arr={5,2,6,3,6,1};
+    Node* y= new Node(arr[2]);
+    cout<<y->data<<endl;
+    cout<<y;
 }
